db6449f9dfe2e26480fc27d2390426443ff2fea8
Marcello Gecchele <10898872+CarbonaraJack@users.noreply.github.com>
2020-11-26 01:32:24 +0900

    Migration to OO model (data-objects.ts)
    Changes to be committed:
    modified:   src/app/data-objects.ts
    modified:   src/app/document-viewer/document-viewer.component.html
    modified:   src/app/document-viewer/document-viewer.component.ts
    modified:   src/custom-theme.scss

--- src/app/data-objects.ts
+++ src/app/data-objects.ts
@@ -1,11 +1,166 @@
-class Word {
-  
+const punctuation : string = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+export class Word {
+  text : string;
+  color : string;     //mat-color property
+  iu : IdeaUnit;      //parent iu
+  seg : Segment;      //parent segment
+  selected : boolean; //html selected property
+
+  constructor(text: string){
+    this.text = text;
+    this.color = "primary";
+    this.selected = true;
+  }
 }
 
-class Segment {
+export class Sent {
+  words : Word[];
+  constructor(){
+    this.words = [];
+  }
 
+  getText() : string {
+    let res : string = ""
+
+    for (let word of this.words){
+      let spacer: string = " ";
+      if (punctuation.includes(word.text)){
+        spacer = "";
+      }
+      res = res + spacer + word.text;
+    }
+    return res.trim();
+  }
 }
 
-class IdeaUnit {
+export class Segment {
+  words: Word[];      //array of child words
+  iu : IdeaUnit;      //the parent iu
+  selected: boolean;  //html selected property
 
+  constructor(){
+    this.words = [];
+    this.selected = false;
+  }
+
+  getText(printIuLabel = false) : string {
+    let res : string = ""
+
+    for (let word of this.words){
+      let spacer: string = " ";
+      if (punctuation.includes(word.text)){
+        spacer = "";
+      }
+      res = res + spacer + word.text;
+    }
+    if (printIuLabel){
+      if (this.iu.disc){
+        res = this.iu.label + "|" + res;
+      }
+    }
+    return res.trim();
+  }
+}
+
+export class IdeaUnit {
+  label: string;      //IU label
+  disc : boolean;     //discontinuous flag
+  childSegs : Set<Segment>; //set of child segments
+
+  constructor(label: string, disc : boolean){
+    this.label = label;
+    this.childSegs = new Set();
+    this.disc = disc;
+  }
+}
+
+export class IUCollection {
+  ius: Map<string, IdeaUnit>;
+  segs: Segment[];
+  sents: Sent[];
+  words: Word[];
+  doc_name: string;
+  doc_type: string;
+
+  constructor(){
+    this.cleanup();
+  }
+
+  private cleanup(){
+    this.ius = new Map<string, IdeaUnit>();
+    this.segs = [];
+    this.sents = [];
+    this.words = [];
+  }
+
+  private parseFile(text: object){
+    console.log(text);
+    this.doc_name = text["doc_name"];
+    this.doc_type = text["doc_type"];
+
+    //seg boundary spy
+    let prev_label : string = "";
+    for (var read_sent of text["sents"]){
+      //initializing Sent object
+      let temp_sent : Sent = new Sent();
+      for (var read_word of read_sent["words"]){
+        //initializing Word object
+        let temp_word : Word = new Word(read_word["text"]);
+
+        //automatically generated IUs are prefixed with the "a" letter
+        let iu_label = "a" + read_word["iu_index"];
+        //boundary check
+        if (iu_label != prev_label){
+          //console.log("boundary");
+          prev_label = iu_label;
+          //creating an empty segment
+          let temp_seg = new Segment();
+          this.segs.push(temp_seg);
+        }
+        //the current segment is always the last
+        let cur_seg = this.segs[this.segs.length-1];
+        //add word to the segment
+        cur_seg.words.push(temp_word);
+        //link the segment to the word
+        temp_word.seg = cur_seg;
+
+        if (!this.ius.has(iu_label)){
+          //console.log("iu not in memory");
+          //creating an empty segment
+          let temp_IU = new IdeaUnit(iu_label,read_word["disc"]);
+          this.ius.set(iu_label,temp_IU);
+        }
+        //adding segment to IU structure
+        let cur_IU = this.ius.get(iu_label);
+        cur_IU["childSegs"].add(cur_seg);
+
+        //adding IU references to a word and segment structure
+        cur_seg.iu = cur_IU;
+        temp_word.iu = cur_IU;
+
+        //adding Word object to memory
+        temp_sent.words.push(temp_word);
+        this.words.push(temp_word);
+      }
+      //adding Sent object to memory
+      this.sents.push(temp_sent);
+    }
+
+
+    console.log(this);
+  }
+
+  readDocument(file : object){
+    this.cleanup();
+    this.parseFile(file);
+  }
+
+  getText(): string {
+    let resText : string  = "";
+    for (let sent of this.sents){
+      resText = resText + "\n" + sent.getText();
+    }
+    return resText.trim();
+  }
 }
--- src/app/document-viewer/document-viewer.component.ts
+++ src/app/document-viewer/document-viewer.component.ts
@@ -10,7 +10,8 @@ import {
   ElementRef
 } from '@angular/core';
 import { ComponentPortal, Portal, TemplatePortal } from '@angular/cdk/portal';
-import { TextService } from '../text.service'
+import { Word, Sent, Segment, IdeaUnit, IUCollection } from '../data-objects';
+import { TextService } from '../text.service';
 
 
 @Component({
@@ -21,21 +22,9 @@ import { TextService } from '../text.service'
 export class DocumentViewerComponent implements OnInit {
 
   @Input() text = {};
-  words : Object[] = [];
-  word_count : Number = null;
-  segs : Object[] = [];
-  sents : Object[] = [];
-  ius : Object = {};
   selected_iu : string = null;
   bubbleMode : string = "iu";
-
-  clearData(): void {
-    this.words = [];
-    this.segs = [];
-    this.sents = [];
-    this.ius = {};
-    this.selected_iu = null;
-  }
+  doc : IUCollection = new IUCollection();
   //max_auto_iu_index : Number = 0;
   //max_man_iu_index : Number = 0;
 
@@ -52,48 +41,6 @@ export class DocumentViewerComponent implements OnInit {
   // !-- PRINTERS --!
   punctuation : string = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
-  getSegText(segment, iuMode = false) : string {
-    let res : string = ""
-
-    for (let w_idx of segment["words"]){
-      let word : string = this.words[w_idx]['text'];
-      let spacer: string = " ";
-      if (this.punctuation.includes(word)){
-        spacer = "";
-      }
-      res = res + spacer + word;
-    }
-    if (iuMode){
-      let iu = this.ius[segment.iu_label]
-      if (iu.disc){
-        res = segment.iu_label + "|" + res;
-      }
-    }
-    return res.trim();
-  }
-
-  getSentText(sent) : string {
-    let res : string = ""
-
-    for (let seg_idx of sent){
-      let seg_text : string = this.getSegText(this.segs[seg_idx]);
-      let spacer: string = " ";
-      if (this.punctuation.includes(seg_text)){
-        spacer = "";
-      }
-      res = res + spacer + seg_text;
-    }
-    return res.trim();
-  }
-
-  getDocText(): string {
-    let resText : string  = "";
-    for (let sent of this.sents){
-      resText = resText + "\n" + this.getSentText(sent);
-    }
-    return resText.trim();
-  }
-
   //procced when the file changes (file upload)
   ngOnChanges(changes: SimpleChanges) {
     for (const propName in changes) {
@@ -105,76 +52,11 @@ export class DocumentViewerComponent implements OnInit {
       console.log("b");
       */
       if (propName == "text"){
-        this.clearData();
-        //loading document from JSON structure
-        if (this.text){
-          console.log(this.text)
-          let prev_w_label = "";
-
-          for (var s of this.text["sents"]){
-            let sent : Object [] = [];
-            for (var w of s["words"]){
-
-              let iu_label = "a" + w.iu_index;
-              if (iu_label != prev_w_label){
-                //segment boundary detected
-                prev_w_label = iu_label
-                this.segs.push({
-                  'iu_label' : iu_label,
-                  'selected': false,
-                  'words' : []
-                });
-                sent.push(this.segs.length-1);
-                //console.log("idx: "+this.segs.length-1);
-              }
-              let seg_idx = this.segs.length-1;
-              let seg = this.segs[seg_idx];
-
-              if (!(iu_label in this.ius)){
-                //console.log("IU not in dictionary");
-                this.ius[iu_label]={
-                  'segs' : new Set(),
-                  'disc' : w.disc
-                };
-                this.ius[iu_label].segs.add(this.segs.length-1);
-              }else{
-                this.ius[iu_label].segs.add(this.segs.length-1);
-              }
-
-
-              //console.log(w);
-              let word : Object = {
-                'text' : w.text,
-                'iu_label' : iu_label,
-                'seg_idx' : seg_idx,
-                'color' : "primary",
-                'selected' : true
-              };
-              this.words.push(word);
-              if(w.word_index!=this.words.length-1){
-                console.log("ERROR: Word index mismatch");
-              }
-              //add word to segment
-              seg['words'].push(this.words.length-1);
-            }
-
-            this.sents.push(sent);
-          }
-        }
+        this.doc.readDocument(this.text);
       }
-      /*
-      for (let key in this.words){
-        console.log(this.words[key]);
-      }
-      */
-      //console.log(this.disc_labels)
-      console.log(this.ius);
-      console.log(this.sents);
-      console.log(this.segs);
-      console.log(this.words);
-
     }
   }
+
   /**
   getText(): void {
     this.textService.getText()
@@ -188,7 +70,8 @@ export class DocumentViewerComponent implements OnInit {
   allowIuCreation : boolean = false;
 
   bubbleClick(seg) : void {
-    this.toggleIuSelect(seg.iu_label);
+    console.log(seg);
+    this.toggleIuSelect(seg.iu.label);
 
     if(this.selected_iu == null){
       this.allowIuEdit = false;
@@ -202,37 +85,36 @@ export class DocumentViewerComponent implements OnInit {
   toggleIuSelect(iu_label) : void {
     if (this.selected_iu != null){
       if (this.selected_iu != iu_label){
-        for (var seg_idx of this.ius[this.selected_iu].segs){
-          this.segs[seg_idx]['selected'] = false;
+        for (var seg of this.doc.ius.get(this.selected_iu).childSegs){
+          seg['selected'] = false;
         }
-        for (var seg_idx of this.ius[iu_label].segs){
-          this.segs[seg_idx]['selected'] = true;
+        for (var seg of this.doc.ius.get(iu_label).childSegs){
+          seg['selected'] = true;
         }
         this.selected_iu = iu_label;
       }else{
-        for (var seg_idx of this.ius[iu_label].segs){
-          this.segs[seg_idx]['selected'] = false;
+        for (var seg of this.doc.ius.get(iu_label).childSegs){
+          seg['selected'] = false;
         }
         this.selected_iu = null;
       }
     }else{
-      for (var seg_idx of this.ius[iu_label].segs){
-        this.segs[seg_idx]['selected'] = true;
+      for (var seg of this.doc.ius.get(iu_label).childSegs){
+        seg['selected'] = true;
       }
       this.selected_iu = iu_label;
     }
   }
 
-  originalWordSet : Object[] = [];
-  removeWordSet : Object[] = [];
-  addWordSet : Object[] = [];
+  originalWordSet : Word[] = [];
+  removeWordSet : Word[] = [];
+  addWordSet : Word[] = [];
 
   engageEditIuMode() : void {
     console.log("Trying to edit Segment "+ this.selected_iu);
     this.bubbleMode = "word";
-    for (let seg_idx of this.ius[this.selected_iu]['segs']){
-      for (let word_idx of this.segs[seg_idx]['words']){
-        let word = this.words[word_idx];
+    for (let seg of this.doc.ius.get(this.selected_iu).childSegs){
+      for (let word of seg['words']){
         word['color'] = 'accent';
         this.originalWordSet.push(word);
       }
@@ -259,7 +141,7 @@ export class DocumentViewerComponent implements OnInit {
       if(!this.addWordSet.includes(word)){
         //add it to the selection set
         this.addWordSet.push(word);
-        word['color'] = 'warn';
+        word['color'] = 'lime-bubble';
       }else{
         //remove the word from the selection set
         this.addWordSet.splice(word);
@@ -270,9 +152,7 @@ export class DocumentViewerComponent implements OnInit {
 
   iuEditSave(): void{
     //save the edits
-    for (let removeWord of this.removeWordSet){
-      let tempSeg = removeWord['seg_idx'];
-    }
+
     //return to IU mode
     this.iuEditCancel();
   }
@@ -290,15 +170,19 @@ export class DocumentViewerComponent implements OnInit {
     for (let word of this.originalWordSet){
       word['color'] = 'primary';
     }
+    for (let word of this.addWordSet){
+      word['color'] = 'primary';
+    }
     this.originalWordSet = [];
     this.removeWordSet = [];
     this.addWordSet = [];
 
     //deselect the IU
-    for (var seg_idx of this.ius[this.selected_iu].segs){
-      this.segs[seg_idx]['selected'] = false;
+    for (var seg of this.doc.ius.get(this.selected_iu).childSegs){
+    seg['selected'] = false;
     }
     this.selected_iu = null;
+    this.allowIuEdit = false;
 
     //enter IU mode
     this.bubbleMode = 'iu';
