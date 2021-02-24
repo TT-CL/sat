e3d95db62aff98815bd2d78176694d42286c5c40
Marcello Gecchele <10898872+CarbonaraJack@users.noreply.github.com>
2021-02-24 13:47:26 +0900

    Split docker-file in multiple files to differentiate prod and dev. Added .env.example to set up docker environment variables
    Removed `/production/` directory.
    Now production instructions are handled via multiple docker-compose
    files.
    
    Common docker-compose instructions are stored in
     - `docker-compose.yml`
    Development docker-compose instructions are stored in
     - `docker-compose.override.yml`
    Docker-compose will load these automatically.
    Production instructions are stored in
     - `production.yml`
    These need to be loaded with the `-f` option.
    A nice aliasing shell script is provided for production
    Run `./prod-compose build` or `./prod-compose up -d` for deployement.
    
    Make sure to copy and populate the .env.example when in production, or
    compose will fail to run.
    
    Mini fix to exposed production port in web's standard Dockerfile.
    
    Changes to be committed:
    new file:   .env.example
    modified:   .gitignore
    new file:   docker-compose.override.yml
    modified:   docker-compose.yml
    modified:   front-end/Dockerfile
    new file:   prod-compose.sh
    new file:   production.yml
    deleted:    production/docker-compose.yml
    deleted:    production/init-letsencrypt.sh.example
    deleted:    production/nginx/Dockerfile
    deleted:    production/nginx/nginx.config.example

---
+++ prod-compose.sh
@@ -0,0 +1,5 @@
+# This shell script is a simple alias for docker compose
+# It merges the main docker-compose.yml file with the production.yml file
+# and passes any arguments as is.
+# You can view it as an alias for `docker-compose`
+docker-compose -f docker-compose.yml -f production.yml "$@"
