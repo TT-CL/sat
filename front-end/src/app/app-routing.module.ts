52f50c9b9f274f1ce124d708999d6f4374b9fd00
CarbonaraJack <10898872+CarbonaraJack@users.noreply.github.com>
2026-04-01 18:15:27 +0900

    Successfully updated angular to V15
    Changes to be committed:
    modified:   package-lock.json
    modified:   package.json
    modified:   src/app/app-routing.module.ts
    modified:   src/app/editor/iu-display/iu-display.component.html
    modified:   src/app/editor/project-downloader/project-downloader.component.html
    modified:   src/app/editor/project-manager/project-manager.component.html
    modified:   src/app/editor/source-card/source-card.component.html
    modified:   src/app/editor/source-editor/source-editor.component.html
    modified:   src/app/editor/source-iu/source-iu.component.html
    modified:   src/app/editor/source-link/source-link.component.html
    modified:   src/app/editor/spinner-overlay/spinner-overlay.component.html
    modified:   src/app/editor/summary-card/summary-card.component.html
    modified:   src/app/editor/summary-editor/summary-editor.component.html
    modified:   src/app/editor/summary-iu/summary-iu.component.html
    modified:   src/app/editor/summary-link/summary-link.component.html
    modified:   src/app/guide/guide.component.sass
    modified:   src/app/user-area/catch-login/catch-login.component.html
    modified:   src/app/user-area/login-page/login-page.component.html
    modified:   src/app/user-area/new-project/new-project.component.html
    modified:   src/app/user-area/project-dash/project-dash.component.html
    modified:   src/app/user-area/upload-overlay/upload-overlay.component.html
    modified:   src/app/user-unauthorized/user-unauthorized.component.html
    modified:   src/custom-theme.scss

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -13,39 +13,44 @@ import { AuthGuardService } from './auth-guard.service';
 import { GuideComponent } from './guide/guide.component';
 
 const routes: Routes = [
-  { path: '', component: LoginPageComponent },
-  { path: 'unauthorized', component: UserUnauthorizedComponent },
-  { path: 'login', component: LoginPageComponent },
+  { path: '', component: LoginPageComponent, title: 'Login Page Component'},
+  { path: 'unauthorized', component: UserUnauthorizedComponent , title: 'Unauthorized'},
+  { path: 'login', component: LoginPageComponent , title: 'Login'},
   //page called upon receiving the OAuth token from Google
-  { path: 'logged-in', component: CatchLoginComponent },
+  { path: 'logged-in', component: CatchLoginComponent , title: 'Login successful'},
   //{ path: '', redirectTo: '/projects', pathMatch: 'full' },
-  { path: 'guide', component: GuideComponent },
+  { path: 'guide', component: GuideComponent , title: 'Guide'},
   {
     path: 'projects',
     component: ProjectDashComponent,
     canActivate: [AuthGuardService],
+    title: 'Projects'
   },
   {
     path: 'projects/new-project',
     component: NewProjectComponent,
     canActivate: [AuthGuardService],
+    title: 'New Project'
   },
   {
     path: 'editor',
     canActivate: [AuthGuardService],
+    title: 'Editor',
     children:[
       {
         path: '',
         redirectTo: '/editor/reader',
-        pathMatch: 'full'
+        pathMatch: 'full',
+        title: 'Reader'
       },
       {
         path:':view',
         component: EditorDashboardComponent,
+        title: 'View'
       }
     ]
   },
-  { path: '**', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent , title: '404'},
 ];
 
 @NgModule({
