1eb0d92d45f11995c17e7b776d5550360c9d29d2
CarbonaraJack <10898872+CarbonaraJack@users.noreply.github.com>
2026-05-06 20:48:23 +0900

    Pushing mongo to version 8
    Changes to be committed:
    modified:   db/init/init.sh
    modified:   docker-compose.yml

--- db/init/init.sh
+++ db/init/init.sh
@@ -1,4 +1,4 @@
-mongo -- "$DB_NAME" <<EOF
+mongosh -- "$DB_NAME" <<EOF
     var user = '$DB_USER';
     var passwd = '$DB_PASSWORD';
     db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
